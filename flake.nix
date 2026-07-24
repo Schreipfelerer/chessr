9139a90ca5e23243d9545f01311d1c07f5ee4925
Schreipfelerer <Schreipfelerer@gmail.com>
2026-07-24 23:42:13 +0200

    flake + envrc

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Rust Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.rust-bin.stable.latest.complete
+            pkgs.cargo-watch
+          ];
+        };
+      }
+    );
+}
