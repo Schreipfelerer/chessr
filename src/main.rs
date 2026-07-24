c1ed6bcbd7705cb4927c45e0411ae55c12066063
Schreipfelerer <Schreipfelerer@gmail.com>
2026-07-25 01:05:55 +0200

    basic authentification

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use reqwest::blocking::Client;
+
 mod helper;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let token = helper::get_lichess_token();
-    println!("Lichess Token: {}", token);
+
+    let client = Client::new();
+
+    let res = client
+        .get("https://lichess.org/api/account")
+        .header("Authorization", format!("Bearer {}", token))
+        .send()?
+        .text()?;
+
+    println!("Response: {}", res);
+
+    Ok(())
 }
